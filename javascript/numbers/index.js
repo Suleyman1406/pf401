7e5b6c2e498740d0e7930b877676822176652748
Suleyman Dadashov <dadasovsuleyman126@gmail.com>
2024-04-27 12:19:05 +0400

    js numbers added

---
+++ javascript/numbers/index.js
@@ -0,0 +1,37 @@
+// let num = 100_000;
+// let num = 100000;
+// let num = 1e5;
+// console.log(num);
+
+// let text = +"12";
+
+// console.log(isNaN(text));
+// console.log(isNaN("12"));
+
+// console.log(-2 / 0);
+
+// console.log(isFinite(2 / 0));
+
+// console.log(Number("10px"));
+
+// console.log(parseInt(10.88));
+// console.log(parseFloat(10.0));
+
+// console.log(parseInt(Math.random() * 100));
+
+// console.log(Math.max(99, 88, 22, 33, 22, 11, 35, 12, 492, 23, 12.4));
+// console.log(Math.min(99, 88, 22, 33, 22, 11, 35, 0, 12, 492, 23, 12.4));
+
+// console.log(Math.pow(3, 3));
+
+console.log(Math.ceil(10.3));
+console.log(Math.ceil(10.9));
+console.log(Math.ceil(10.5));
+console.log("--------");
+console.log(Math.floor(10.3));
+console.log(Math.floor(10.9));
+console.log(Math.floor(10.5));
+console.log("--------");
+console.log(Math.round(10.3));
+console.log(Math.round(10.9));
+console.log(Math.round(10.5));
