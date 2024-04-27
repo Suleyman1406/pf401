483fd4bbbc298b41f5aea427fe2193cdf6b0238f
Suleyman Dadashov <dadasovsuleyman126@gmail.com>
2024-04-27 13:33:31 +0400

    js strings added

---
+++ javascript/strings/index.js
@@ -0,0 +1,52 @@
+// let str = `Suleymans \\`;
+// console.log(str);
+
+// let str = "Yusif ";
+// console.log(str.length);
+// console.log(str[0]);
+// console.log(str[99]);
+
+
+// let str = "İ I i ı Hello  Code Academy World!";
+
+// str = str.toUpperCase();
+
+// console.log(str);
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+// console.log(str.toLocaleUpperCase("AZ-az"));
+// console.log(str.toLocaleLowerCase("AZ-az"));
+
+// let str = "Hello Code Academy World!";
+// console.log(str.indexOf("Code"));
+// console.log(str.lastIndexOf("o"));
+
+// console.log(str.includes("A", 18));
+
+// console.log(str.startsWith("Hello"));
+// console.log(str.endsWith("World!"));
+
+// console.log( str.replace(" ", "-"))
+// console.log(str.replaceAll(" ", "_"))
+
+
+// let str = "    Hello Code Academy World!   ";
+
+// console.log(str.trim()+"123");
+// console.log(str.trimStart()+"123");
+// console.log(str.trimEnd()+"123");
+
+
+// let str = "salam_necesen_ne_var_ne_yox";
+// let str = "Hello Code Academy World!";
+
+
+// console.log(str.split(""));
+
+let str = "Salam necesen";
+
+// console.log(str.slice(-4, -1));
+
+
+// console.log(str.substr(2, 3));
