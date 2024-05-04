6a8f4a427a6cdfd358e2143aa702c207bac2c859
Suleyman Dadashov <dadasovsuleyman126@gmail.com>
2024-05-04 11:26:33 +0400

    js array methods added

--- javascript/arrays/index.js
+++ javascript/arrays/index.js
@@ -40,7 +40,6 @@
 //   console.log(`${items[i]} - ${count} eded`);
 // }
 
-
 // let myArr = [1,"sd",true, []]
 
 // let numbers = [3,4,5,6,12,23,23,45,5]
@@ -50,8 +49,7 @@
 // console.log(numbers.at(5));
 // console.log(numbers.at(-4));
 
-
-let nums = [1,2,3,4,5,99];
+let nums = [1, 2, 3, 4, 5, 99];
 
 // nums.push(6)
 // nums.push(6)
@@ -65,18 +63,14 @@ let nums = [1,2,3,4,5,99];
 // let result = nums.pop()
 // console.log(result);
 
-
 // nums.shift()
 // nums.shift()
 
-// console.log(nums); 
-
+// console.log(nums);
 
 // [1, 2, 3, 4]
 // [1, 2, 3, 4, 5]
 // [5, 1, 2, 3, 4]
-
-
 
 // Referance
 
@@ -87,14 +81,12 @@ let nums = [1,2,3,4,5,99];
 
 // console.log(b);
 
-
 // let a = [1,2,3];
 // let b = a;
 
 // a.push(4);
 
 // console.log(b);
-
 
 // let a = [1, 2, 3];
 
@@ -104,21 +96,15 @@ let nums = [1,2,3,4,5,99];
 
 // console.log(b);
 
-
-
-
-
 // let a = [1,2,3];
 
-// function test(data){ 
+// function test(data){
 //   data = [1,2,3,4]
 // }
 
 // test(a);
 
 // console.log(a);
-
-
 
 // let numbers = [11,22,33,44,55,66];
 
@@ -133,22 +119,17 @@ let nums = [1,2,3,4,5,99];
 //   console.log(index);
 // }
 
-
 // let a = [];
 
 // let a = new Array(1).fill(22);
 // console.log(a);
 
-
 // console.log("" == 0);
-
 
 // let numbers = [0,11,22,33,44,55,66]; // 77
 
-
 // console.log(numbers.slice(1,3));
 // console.log(numbers);
-
 
 // let res = numbers.splice(numbers.length, 0,77);
 
@@ -156,14 +137,14 @@ let nums = [1,2,3,4,5,99];
 
 // console.log(numbers);
 
-// let numbers = [0,11,22,33,44,55,66,44]; 
+// let numbers = [0,11,22,33,44,55,66,44];
 
 // console.log(numbers.concat([3,4,5,9]));
 
 // console.log(numbers.indexOf(44));
 // console.log(numbers.lastIndexOf(44));
 
-// let numbers = [0,11,22,33,44,55,66,44]; 
+// let numbers = [0,11,22,33,44,55,66,44];
 
 // numbers.reverse();
 
@@ -173,10 +154,129 @@ let nums = [1,2,3,4,5,99];
 
 // console.log(res);
 
-let names = [
-  "su$leyman%dadas$ov",
-  "emir$aslan%omer$ov",
-]
+// let names = [
+//   "su$leyman%dadas$ov",
+//   "emir$aslan%omer$ov",
+// ]
 
+// console.log(names.join("/").toUpperCase().replaceAll("%", " ").replaceAll("$",""))
 
-console.log(names.join("/").toUpperCase().replaceAll("%", " ").replaceAll("$",""))
+// let arr = [1, 2, 3, 4];
+
+// for(let i = 0; i < arr.length; i++){
+//   console.log(arr[i]);
+// }
+
+// for(let n of arr){
+//   console.log(n);
+// }
+
+// arr.forEach((value, i, arr) => {
+//   if (value % 2 === 0) {
+//     console.log(value, i, arr);
+//   }
+// });
+
+// let arr = [1, 2, 3, 4];
+// let res = arr.map((val) => {
+//   if (val % 2 === 0) {
+//     return val * 2;
+//   }
+// });
+
+// console.log(res);
+
+// arr.map()
+
+// [ 2, 4, 6, 8]
+// // |
+// // map
+// // |
+// [ 4, 8, 12, 16]
+
+// let arr = [3, 9, 11, 5, 8, 19];
+
+// let res = arr.find((val) => {
+//   // if (val % 5 === 0) {
+//   //   return true;
+//   // }
+//   return val % 12 === 0;
+// });
+
+// console.log(res);
+
+// let arr = [1, 2, 5, 0, 12, 15];
+
+// let res = arr.find((item) => {
+//   return !item;
+// });
+// let res2 = arr.find((item) => {
+//   return item;
+// });
+
+// console.log(res);
+// console.log(res2);
+
+// let arr = [3, 9, 11, 5, 8, 19];
+
+// let res = arr.findIndex((val) => {
+//   return val > -4;
+// });
+
+// console.log(res);
+
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let res = arr.filter((item, i, items) => {
+//   return items.includes(i);
+// });
+
+// console.log("result: ", res);
+
+// let arr = [2, 4, 6, 8, 10, 1];
+
+// let res = arr.every((item) => item % 2 === 0);
+
+// console.log("result:", res);
+
+// let res = arr.some((item) => item % 2 === 1);
+
+// console.log("result:", res);
+
+// let arr = [5, 4, 1, 10, 11];
+
+// arr.sort((a, b) => {
+//   // if (a > b) {
+//   //   return -1;
+//   // } else if (a < b) {
+//   //   return 1;
+//   // }
+//   // return 0;
+//   return b - a;
+// });
+
+// console.log(arr);
+
+// let numbers = [1, 2, 3, 4, 5, 6];
+
+// let res = numbers.reduce((prev, item, i, items) => {
+//   prev.push(item);
+//   return prev;
+// }, []);
+
+// console.log("result:", res);
+
+// let a = b => b+1
+
+// console.log(a(2));
+
+// let test = (a) => {
+//   return a + 1;
+// };
+
+// let test2 = (a) => a + 1;
+
+// let test3 = a => a + 1;
+// let test4 = (a,b) => a + b;
+
+// console.log(test3(5));
