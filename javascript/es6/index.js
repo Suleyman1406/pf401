4705ef841b82e48d37ceada633769b2ea22b1e0e
Suleyman Dadashov <dadasovsuleyman126@gmail.com>
2024-05-11 15:00:18 +0400

    es6 & oop added

---
+++ javascript/es6/index.js
@@ -0,0 +1,84 @@
+// function (){
+// person = "Ilhan";
+// person = "Ilhan";
+
+// var person = "Ilhan";
+
+// console.log(person);
+// console.log(person);
+
+// Hoisting
+
+// console.log(a);
+
+// let a = 12;
+
+// {
+//   let a = 12;
+
+// }
+// console.log(a);
+
+
+// let isTrue = true;
+// let a;
+
+// if(isTrue){
+//   a = 15;
+// }else{
+//   a = 18
+// }
+
+// console.log(a);
+
+// {
+//   var a = 12;
+
+// }
+// console.log(a);
+
+// let isTrue = false;
+
+// if(isTrue){
+//   var a = 15;
+// }else{
+//   var a = 18
+// }
+
+// console.log(a);
+
+// let isTrue = false;
+
+// if(isTrue){
+//   var a = 15;
+// }
+
+// console.log(a);
+
+// function test(){
+//   var a = 12;
+// }
+
+// test()
+// console.log(a);
+// }
+
+// IIFE
+
+// (function (name){
+//   console.log("salam",name);
+// })("Suleyman")
+
+
+// class Person{
+//   surname = "Test"
+
+//   constructor(name, surname){
+//     this.name = name;
+//     this.surname = surname
+//   }
+// }
+
+// let person = new Person("Shebnem", "Memmedova");
+
+// console.log(person);
---
+++ javascript/oop/index.js
@@ -0,0 +1,60 @@
+// const person  =  {
+//   name: "Suleyman"
+// }
+
+// function Person(name, surname, birthYear){
+//   this.name = name;
+//   this.surname = surname;
+//   this.birthYear  = birthYear;
+//   this.printAge = function(){
+//     console.log(2024 - this.birthYear);
+//   }
+// }
+
+// const person = new Person("Suleyman","Dadashow", 2012);
+// const person2 = new Person("Emiraslan","Omerov", 2013);
+
+// person.printAge()
+// person2.printAge()
+// console.log(person);
+// console.log(person2);
+
+
+// const person ={
+//   name: "Suleyman",
+//   birthYear: 2012,
+//   calculateAge: function(){
+//     console.log(2024 - this.birthYear);
+//     this.calculateAge()
+//   }
+// }
+
+// person.calculateAge()
+
+
+// function Person(name, surname, birthYear){
+//   this.name = name;
+//   this.surname = surname;
+//   this.birthYear  = birthYear;
+// }
+
+// const person = new Person("Suleyman","Dadashow", 2012);
+// const person2 = new Person("Emiraslan","Omerov", 2013);
+
+// person.printAge()
+// person2.printAge()
+// Person.prototype.printAge = function(){
+//   console.log(2024 - this.birthYear);
+// }
+
+// const person = new Person("Suleyman","Dadashow", 2012);
+// const person2 = new Person("Suleyman","Dadashow", 2012);
+
+// person.printAge();
+// console.log(person);
+// person.printSomething();
+// person2.printSomething();
+
+
+
+
