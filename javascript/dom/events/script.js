239fe55449cf29c8218c2b0eb34ce2f05db88f2b
Suleyman Dadashov <dadasovsuleyman126@gmail.com>
2024-05-12 14:38:29 +0400

    js events added

---
+++ javascript/dom/events/script.js
@@ -0,0 +1,48 @@
+const saveBtnElement = document.getElementById("save-btn");
+
+// Event Handler
+
+// saveBtnElement.onclick = function (e) {
+// saveBtnElement.textContent ="hello"
+// };
+// saveBtnElement.onclick = function () {
+//   console.log("Hadnled Btn 2 Clicked!");
+// };
+// saveBtnElement.onclick = function () {
+//   console.log("Hadnled Btn 3 Clicked!");
+// };
+
+// Event Listener
+// saveBtnElement.addEventListener("click", function () {
+//   console.log("Listener Btn 1 Clicked!");
+// });
+// saveBtnElement.addEventListener("click", function () {
+//   console.log("Listener Btn 2 Clicked!");
+// });
+// saveBtnElement.addEventListener("click", function () {
+//   console.log("Listener Btn 3 Clicked!");
+// });
+
+const titleInputElement = document.getElementById("title-input");
+
+// titleInputElement.addEventListener("keypress", () => {
+//   console.log("key pressed", titleInputElement.value);
+// });
+// titleInputElement.addEventListener("keyup", () => {
+//   console.log("key up", titleInputElement.value);
+// });
+// titleInputElement.addEventListener("keydown", () => {
+//   console.log("key down", titleInputElement.value);
+// });
+// titleInputElement.addEventListener("focus", () => {
+//   console.log("fokus olduq");
+// });
+// titleInputElement.addEventListener("blur", () => {
+//   console.log("cole cixdiq");
+// });
+
+// const boxElement = document.getElementById("box");
+
+// boxElement.addEventListener("dblclick", (e) => {
+//   console.log("dblclick");
+// });
