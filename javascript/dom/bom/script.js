4a90f9c42d9879e0d834315454c6adf9eb75f6b6
Suleyman Dadashov <dadasovsuleyman126@gmail.com>
2024-05-25 12:52:32 +0400

    js bom added

---
+++ javascript/dom/bom/script.js
@@ -0,0 +1,81 @@
+// alert("Hello");
+// console.log("hello");
+
+// let result = confirm("Are you sure to delete your final project?");
+
+// console.log(
+//   result ? "Your project deleted." : "You can continue to work on your project!"
+// );
+// let result = prompt("What is ypur name?");
+
+// console.log(result);
+
+// console.log(
+//   decodeURIComponent(
+//     "https://kontakt.az/iqlim-texnikasi/kondisionerler?kh_stehsal%5B0%5D=Alarko&kh_stehsal%5B1%5D=Beko&kh_stehsal%5B2%5D=AUX"
+//   )
+// );
+
+// window.location.href = "https://translate.google.com";
+
+// const btn = document.getElementById("btn");
+
+// btn.addEventListener("click", () => {
+//   window.location.reload();
+// });
+
+// console.log(window.history[0]);
+
+// console.log("innerWidth", window.innerWidth);
+
+// window.addEventListener("resize", () => {
+//   // console.log("inner", window.innerWidth, window.innerHeight);
+//   // console.log("outer", window.outerWidth, window.outerHeight);
+
+//   console.log(window.screen);
+// });
+
+// setTimeout(() => {
+//   console.log("Salam aleykum");
+// }, 2000);
+
+// console.log("Necesen?");
+
+// setInterval(() => {
+//   console.log("Salam");
+//   let a = 1433;
+//   let b = 888 * a;
+
+//   console.log(a - b + b * b);
+// }, 1000);
+
+// let count = 0;
+
+// setInterval(() => {
+//   console.log(count++);
+// }, 100);
+
+// setInterval(() => {
+//   console.log("Salam");
+// }, 10);
+
+// setTimeout(() => {
+//   console.log("Salam");
+// }, 10);
+
+// for (let i = 0; i < 100000; i++) {
+//   console.log(i);
+// }
+
+// let intervalId = setInterval(() => {
+//   console.log("hello");
+//   console.log("------");
+// }, 500);
+
+// let timeOutId = setTimeout(() => {
+//   clearInterval(intervalId);
+// }, 3000);
+
+// setTimeout(() => {
+//   clearTimeout(timeOutId);
+// }, 4000);
